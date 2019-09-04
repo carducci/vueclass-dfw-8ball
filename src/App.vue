bafc5139ccf375cf8b3b3a2777823248c8a8e779
Vue Workshop <example@example.com>
2019-09-04 15:00:13 -0500

    Create ball component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
+    <TheBall></TheBall>
   </div>
 </template>
 
 <script>
-
+  import TheBall from '@/components/TheBall';
+  export default {
+    components:{
+      TheBall
+    }
+  }
 </script>
 
 <style>
